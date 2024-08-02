5f680bfe7c146a023abe4a2f3cccafbb9b24d19d
emmamitm <emmamit.co@gmail.com>
2024-08-01 19:52:10 -0600

    vizualize

--- User.py
+++ User.py
@@ -1,4 +1,5 @@
 from CSV import CSVReader, Movie
+from graphviz import Digraph
 class Node:
     def __init__(self, score, movie):
         self.score = score
@@ -6,7 +7,6 @@ class Node:
         self.height = 1
         self.left = None
         self.right = None
-
 class User:
     def __init__(self, filename, genre, rating_high, rating_low, runtime_high, runtime_low,
                  meta_score, plot, watched_movies, tags):
@@ -20,16 +20,12 @@ class User:
         self.plot = plot
         self.watched_movies = set(watched_movies)
         self.tags = set(tags)
-
     def get_movies(self):
         return self.reader.Movies
-
     def add_watched_movie(self, movie):
         self.watched_movies.add(movie)
-
     def add_tag(self, tag):
         self.tags.add(tag)
-
 class Node:
     def __init__(self, score, movie):
         self.score = score
@@ -37,8 +33,6 @@ class Node:
         self.height = 1
         self.left = None
         self.right = None
-
-
 class MoviesDB:
     def __init__(self, user):
         self.user = user
@@ -46,16 +40,12 @@ class MoviesDB:
         self.root = None
         self.number = 0
         self.initialize_binary_tree()
-        #self.titles = []
-        #self.print_inorder(self.root, self.titles)
         self.print_best_order(self.root)
-
+        self.visualize_tree(self.root, 'bst')
     def initialize_binary_tree(self):
         for i, movie in enumerate(self.movies):
-            #number = self.score_calculator(movie)
-            number =i
+            number = self.score_calculator(movie)
             self.insert(number, movie)
-
     def score_calculator(self, movie):
         rating_portion = ((self.user.rating_high - self.user.rating_low) -
                           min(self.user.rating_high - movie.rating,
@@ -71,7 +61,6 @@ class MoviesDB:
                         genre_portion +
                         1.0)
         return score
-
     def insert(self, score, movie):
         if not self.root:
             self.root = Node(score, movie)
@@ -87,25 +76,16 @@ class MoviesDB:
             node.right = self.insert_helper(node.right, score, movie)
         else:
             return node
-
         node.height = 1 + max(self.height(node.left), self.height(node.right))
-
-        # Left tree is heavy
         if self.get_balance(node) > 1:
-            # Left right case
             if score > node.left.score:
                 node.left = self.rotate_left(node.left)
-            # Left left case
             node = self.rotate_right(node)
         elif self.get_balance(node) < -1:
-            # Right left case
             if score < node.right.score:
                 node.right = self.rotate_right(node.right)
-            # Right right case
             node = self.rotate_left(node)
-
         return node
-
     def rotate_left(self, root):
         new_root = root.right
         root.right = new_root.left
@@ -113,7 +93,6 @@ class MoviesDB:
         root.height = max(self.height(root.left), self.height(root.right)) + 1
         new_root.height = max(self.height(new_root.left), self.height(new_root.right)) + 1
         return new_root
-
     def rotate_right(self, root):
         new_root = root.left
         root.left = new_root.right
@@ -121,10 +100,8 @@ class MoviesDB:
         root.height = max(self.height(root.left), self.height(root.right)) + 1
         new_root.height = max(self.height(new_root.left), self.height(new_root.right)) + 1
         return new_root
-
     def get_balance(self, node):
         return self.height(node.left) - self.height(node.right)
-
     def height(self, node):
         if not node:
             return 0
@@ -136,12 +113,21 @@ class MoviesDB:
             self.print_best_order(node.left)
     def count_nodes(self, node):
         return 1 + self.count_nodes(node.left) + self.count_nodes(node.right) if node else 0
-
     def print_inorder(self, node, titles):
         if node:
             self.print_inorder(node.left, titles)
             titles.append(node.movie.title)
             self.print_inorder(node.right, titles)
-
-
-
+    def visualize_tree(self, node, filename):
+        dot = Digraph()
+        self.add_nodes(dot, node)
+        dot.render(filename, format='png', view=True)
+    def add_nodes(self, graph, node):
+        if node:
+            graph.node(str(node.score), node.movie.title)
+            if node.left:
+                graph.edge(str(node.score), str(node.left.score))
+                self.add_nodes(graph, node.left)
+            if node.right:
+                graph.edge(str(node.score), str(node.right.score))
+                self.add_nodes(graph, node.right)
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-
 from User import User, MoviesDB
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     m = set()
     user1 = User("CSV.csv", "action", 10, 5, 120, 90, 0, "h", m, m)
     movie = MoviesDB(user1)
-
